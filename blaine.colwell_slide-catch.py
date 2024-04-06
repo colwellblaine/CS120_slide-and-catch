0dc4dd9db3be54641def7651d85b73f40f7e251a
Blaine Colwell <36966010+colwellblaine@users.noreply.github.com>
2024-04-05 23:24:37 -0400

    Update blaine.colwell_slide-catch.py

--- blaine.colwell_slide-catch.py
+++ blaine.colwell_slide-catch.py
@@ -1,13 +1,10 @@
 import simpleGE, pygame, random
 
-# need to get rid of these; currently only here for the label
-SCREEN_HEIGHT = 480
-SCREEN_WIDTH = 640
+# I was working on moving these into their individual classes, but I don't think I'll have time
+PLAYER_MOVE_SPEED = 0.02 * 640
+COIN_MOVE_SPEED = 0.005 * 640
 
-# in theory, should give consistent movement across any given screen
-# definitely a better way to do this
-PLAYER_MOVE_SPEED = 0.02 * SCREEN_WIDTH
-COIN_MOVE_SPEED = 0.005 * SCREEN_WIDTH
+
 
 class Game(simpleGE.Scene):
     def __init__(self):
@@ -15,11 +12,12 @@ class Game(simpleGE.Scene):
         # create player object
         self.player = Player(self)
         self.setImage("assets/bg.png")
-
+        
+        #score, timer, and sound
         self.score_label = ScoreLabel()
         self.time_label = TimeLabel()
         self.timer = simpleGE.Timer()
-        self.timer.totalTime = 5
+        self.timer.totalTime = 30
         self.score = 0
         self.collect_sound = simpleGE.Sound("assets/pickup0.wav")
 
@@ -31,6 +29,7 @@ class Game(simpleGE.Scene):
             self.coins[-1].y = random.randint(-300, 0)
 
         self.sprites = [self.player, self.coins, self.score_label, self.time_label]
+
 
     def process(self):
         # check if each coin collides with player, reset and increment score
@@ -45,7 +44,8 @@ class Game(simpleGE.Scene):
                 self.score_label.text = f"{self.score}"
 
         self.time_label.text = str(self.timer.getTimeLeft())[0:5]
-
+        
+        # actually stop the game at some point
         if self.timer.getTimeLeft() < 0:
             self.stop()
 
@@ -56,7 +56,9 @@ class ScoreLabel(simpleGE.Label):
         self.fgColor = "light green"
         self.clearBack = True
         self.text = "0"
-        self.center = (SCREEN_WIDTH - 20, 20)
+        # I couldn't figure out a good way to get screen width here, so it's just hard set to 640
+        self.center = (640 - 20, 20)
+        
         self.size = (150, 30)
 
 
@@ -73,24 +75,25 @@ class TimeLabel(simpleGE.Label):
 class Player(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        # this was a placeholder, but now it's not
+        # the guy
         self.setImage("assets/character.png")
         self.y = self.screenHeight - (self.screenHeight * 0.08)
+        self.move_speed = 0.02 * self.screenHeight
 
 
     def update(self):
-        if self.isKeyPressed(pygame.K_a):
-            self.x -= PLAYER_MOVE_SPEED
-        if self.isKeyPressed(pygame.K_d):
-            self.x += PLAYER_MOVE_SPEED
-        if self.isKeyPressed(pygame.K_UP):
-            print("up")
-        if self.isKeyPressed(pygame.K_DOWN):
-            print("up")
+        # A/D and left/right both work. Up/down and W/S don't.
+        if self.isKeyPressed(pygame.K_a) or self.isKeyPressed(pygame.K_LEFT):
+            self.x -= self.move_speed
+        if self.isKeyPressed(pygame.K_d) or self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.move_speed
+        #if self.isKeyPressed(pygame.K_UP):
+        #    print("up")
+        #if self.isKeyPressed(pygame.K_DOWN):
+        #    print("up")
 
 
-        # THESE HAVE TO BE CHECKED SEPARATE
-        # otherwise, we'll go off the screen in corners
+        # No escaping
         if self.right >= self.screenWidth:
             self.right = self.screenWidth
         elif self.left <= 0:
@@ -105,12 +108,10 @@ class Player(simpleGE.Sprite):
 class Coin(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        # this is supposed to give them a random x position
-        # it doesn't work at the beginning of the game
         self.setImage("assets/coin2.png")
+        # turns out setting the x value before the image causes the x to be set back to 0
         self.x = random.randint(0, self.screenWidth)
         # get a random float from 0.5 to 1.5; causes coins to move at different speeds
-        # need to find a cleaner way to do this if possible
         self.move_speed = random.uniform(0.5, 1.5) * COIN_MOVE_SPEED
 
     # reset to top
@@ -122,7 +123,7 @@ class Coin(simpleGE.Sprite):
     def update(self):
         self.y += self.move_speed
 
-        if self.top >= SCREEN_HEIGHT:
+        if self.top >= self.screenHeight:
             self.reset()
 
 
@@ -130,28 +131,28 @@ class Coin(simpleGE.Sprite):
 
 
 
-
+#     menu
 class Menu(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.status = ""
 
-
+        # SECOND LINE
         self.instructions = simpleGE.MultiLabel()
         self.instructions.textLines = (
                 "Collect as many coins as possible within the time limit",
                 ""
                 )
-
-        self.instructions.center = (SCREEN_WIDTH / 2, 100)
-        self.instructions.size = (SCREEN_WIDTH, 200)
+        self.instructions.center = (self.background.get_width() / 2, 100)
+        self.instructions.size = (self.background.get_width(), 200)
         self.instructions.fgColor = "light green"
         self.instructions.bgColor = "black"
 
+        # buttons
         self.play_button = simpleGE.Button()
         self.play_button.text = "Play"
         self.play_button.fgColor = "light green"
-        self.play_button.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+        self.play_button.center = ((self.background.get_width() / 2), (self.background.get_height() / 2))
         self.play_button.size = (150, 30)
         self.play_button.clearBack = True
         
@@ -159,15 +160,16 @@ class Menu(simpleGE.Scene):
         self.quit_button.text = "Quit"
         self.quit_button.fgColor = "light green"
         self.quit_button.clearBack = True
-        self.quit_button.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 1.8))
+        self.quit_button.center = ((self.background.get_width() / 2), (self.background.get_height() / 1.8))
 
+        # last score
         self.last_score = simpleGE.Label()
         self.last_score.text = "Previous score: 0"
         self.last_score.size = (250, 25)
         self.last_score.fgColor = "light green"
-        self.last_score.clearBack = True
-        self.last_score.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 100)
-
+        self.last_score.clearBack = True                            # change this
+        self.last_score.center = (self.background.get_width()/2, self.background.get_height()/2 - 100)
+        
 
 
         self.sprites = [self.play_button, self.quit_button, self.instructions, self.last_score]
@@ -191,19 +193,18 @@ class Menu(simpleGE.Scene):
 
 
 if __name__ == "__main__":
+    # run the menu once at the start
     menu = Menu()
     menu.start()
 
     playing = True
     while playing:
-
-
-
         if menu.status == "play":
             game = Game()
             game.start()
 
             menu = Menu()
+            # set last score value
             menu.set_last_score(str(game.score))
             menu.start()
 
